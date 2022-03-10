778a0575ac00c8e046d0a90697deb0c0b823d031
VanesMP <vanessa.MP@outlook.fr>
2022-03-10 21:17:14 +0100

    suppression des console.log

--- kasa/src/Components/SlideShow.jsx
+++ kasa/src/Components/SlideShow.jsx
@@ -5,21 +5,17 @@ import arrowslide from "../assets/arrowSlide.png"
 
 import "../styles/components-style/SlideShow.css"
 
-
 function SlideShow ({views}) {
 
     const [page, setPage] = useState(0)
 
     const nextSlide = () => {
-        console.log('image suivante')
         setPage(page < views.length - 1 ? page + 1 : 0);
     }
     
     const prevSlide = () => {
-        console.log('image précédente')
         setPage(page > 0 ? page - 1 : views.length - 1)
     }
-
 
             return (
                 <section className="sectionSheet">
