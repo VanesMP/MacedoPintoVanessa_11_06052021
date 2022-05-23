28b42a83b8a382377ca4ddddb46120353d8d3cf8
VanesMP <vanessa.MP@outlook.fr>
2022-05-23 23:57:35 +0200

    last changes

--- kasa/src/pages/Home.jsx
+++ kasa/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import '../styles/pages-style/index-style.css';
 import Header from "../Components/Header";
 import Banner from "../Components/Banner";
-import ContainerCards from "../Components/ContainerCards";
+import '../styles/components-style/ContainerCards.css';
+import Cards from '../Components/Cards';
 import Footer from "../Components/Footer";
 
 import background from "../assets/backgroungAccueil.png";
@@ -15,7 +16,9 @@ function Home() {
                   <img className="background" src={background} alt="kasa-location-appartements" />
         <h1 className="slogan">Chez vous, partout et ailleurs</h1> 
                   </Banner> 
-                  <ContainerCards/>
+                  <div className='containerSection'>       
+                  <Cards />
+                  </div>
                   <Footer/>
         </div>
     )
