1c19517df8bb0e8380904827fca33396fb055e5b
VanesMP <vanessa.MP@outlook.fr>
2022-05-23 20:14:32 +0200

    last change

--- kasa/src/Components/Tags.jsx
+++ kasa/src/Components/Tags.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "../styles/components-style/Tags.css"
 
 function Tags({tag}) {
+
     return (
         <ul className="tagList">
-            {tag.map((nameTag, index) => (
-                <li className="containerTag" key={index}>
+            {tag.map((nameTag) => (
+                <li className="containerTag" key={nameTag}>
                     <p className="textTag">{nameTag}</p>
                 </li>
             ))}
